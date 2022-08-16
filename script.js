ca28cbaa83b84cd26bf6f2b06b988f125d2c1ab2
uicafe <58023936+uicafe@users.noreply.github.com>
2022-08-16 22:59:45 +0800

    star

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-var sum = 0;
-for (var i = 1; i <= 100; i++) {
-    sum = sum + i; // sum += i; 或者这样写
+var row = '';
+for (var i = 1; i <= 9; i++){ //行数
+    for (var j = 1; j <= i; j++){ //列数，重点：与上面的区仅是 j <= 1 变为 j <= i
+        row += j + '×' + i + '=' + i * j + '\t';
+    }
+    row = row + '\n';
 }
-console.log(sum);
-
+console.log(row);
