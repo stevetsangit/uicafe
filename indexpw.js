26afb8d9cc316d961b1700c6b53fcce276d02073
uicafe <58023936+uicafe@users.noreply.github.com>
2022-08-18 01:24:45 +0800

    good

--- indexpw.js
+++ indexpw.js
@@ -1,8 +1,19 @@
 
-var pw1 = 'Fung'
-var pw2 = prompt("🔢\n请输入暗语，如果你不知道只能强制关闭浏览器了！哈哈哈😂\nPlease enter the code word, if you don't know, you can only force close the browser! Hahaha\n");
-if (pw2 === pw1) {
-    console('welcome');
-} else {
-    location.reload();
-}
+// var pw1 = 'Fung'
+// var pw2 = prompt("🔢\n请输入暗语，如果你不知道只能强制关闭浏览器了！哈哈哈😂\nPlease enter the code word, if you don't know, you can only force close the browser! Hahaha\n");
+// if (pw2 === pw1) {
+//     console('welcome');
+// } else {
+//     location.reload();
+// }
+
+// var mig = prompt('Please enter the code word');
+// while (mig !== 'UICafe') {
+//     mig = prompt('Please enter the code word');
+// }
+// console.log('Welcome');
+
+do {
+    var mig = prompt("🔢\n请输入暗语，如果你不知道只能强制关闭浏览器了！哈哈哈😂\nPlease enter the code word, if you don't know, you can only force close the browser! Hahaha\n");
+} while (mig !== 'UICafe');
+console.log('Welcome');
