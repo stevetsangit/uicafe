3b51701c589edbda45bd2ccd8d2492f78760e4b1
Steve Tsang <stevagesz@gmail.com>
2024-01-24 02:27:34 +0800

    bbb

---
+++ bmw_test/communitycmslayout/script-tabs.js
@@ -0,0 +1,27 @@
+function openCity(evt, tabs) {
+
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(tabs).style.display = "flex";
+  evt.currentTarget.className += " active";
+
+}
+
+// Get the element with id="defaultOpen" and click on it
+window.onload = function(){
+document.getElementById("defaulttab").click();
+}
